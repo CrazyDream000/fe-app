bbeb4f8647d7565bd89eadea5b4c1daaaa4f1809
DaveVodrazka <dave.vodrazka@gmail.com>
2024-04-08 10:05:44 +0200

    feat: switch to sepolia

--- src/calls/getNonExpiredOptions.ts
+++ src/calls/getNonExpiredOptions.ts
@@ -1,10 +1,11 @@
 import { apiUrl } from "../api";
 import { OptionWithPremia } from "../classes/Option";
+import { NETWORK } from "../constants/amm";
 import { parseBatchOfOptions } from "../utils/optionParsers/batch";
 import { parseNonExpiredOption } from "../utils/optionParsers/parseNonExpiredOption";
 
 export const getNonExpiredOptions = async (): Promise<OptionWithPremia[]> =>
-  fetch(apiUrl("live-options"))
+  fetch(apiUrl("live-options", { network: NETWORK }))
     .then((res) => res.json())
     .then((res) => {
       if (res?.status !== "success" || !res?.data?.length) {
