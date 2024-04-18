39c59464ec1fadcc1f36bd4824175bf74d2e057e
DaveVodrazka <dave.vodrazka@gmail.com>
2024-04-18 23:30:54 +0200

    feat: remove prop 58 add prop 60

--- src/components/Proposal/fetchProposal.ts
+++ src/components/Proposal/fetchProposal.ts
@@ -2,13 +2,13 @@ import { Proposal } from "../../types/proposal";
 
 export const activeProposal: Proposal[] = [
   {
-    id: 58,
-    discordLink:
-      "https://discord.com/channels/969228248552706078/1228017082931740753/1228017085196800020"
-  },
-  {
     id: 59,
     discordLink:
       "https://discord.com/channels/969228248552706078/1230172177303605389/1230172178776064090"
+  },
+  {
+    id: 60,
+    discordLink:
+      "https://discord.com/channels/969228248552706078/1230631204093693993/1230631205880467507"
   }
 ];
