c598bba0d41f94bd959ee30f0ad0416965247340
Ondřej Sojka <8470346+tensojka@users.noreply.github.com>
2024-04-25 14:24:24 +0200

    Add proposal 61

--- src/components/Proposal/fetchProposal.ts
+++ src/components/Proposal/fetchProposal.ts
@@ -2,13 +2,8 @@ import { Proposal } from "../../types/proposal";
 
 export const activeProposal: Proposal[] = [
   {
-    id: 59,
+    id: 61,
     discordLink:
-      "https://discord.com/channels/969228248552706078/1230172177303605389/1230172178776064090"
-  },
-  {
-    id: 60,
-    discordLink:
-      "https://discord.com/channels/969228248552706078/1230631204093693993/1230631205880467507"
+      "https://discord.com/channels/969228248552706078/1233030072055627897/1233030073611718828"
   }
 ];
