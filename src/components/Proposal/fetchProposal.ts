0cd033a6c2b6567c16b85c0e70d4b00506a6ee40
Ondřej Sojka <8470346+tensojka@users.noreply.github.com>
2024-05-15 10:29:54 +0200

    Add proposal 64, remove passed prop 62

--- src/components/Proposal/fetchProposal.ts
+++ src/components/Proposal/fetchProposal.ts
@@ -2,13 +2,13 @@ import { Proposal } from "../../types/proposal";
 
 export const activeProposal: Proposal[] = [
   {
-    id: 62,
-    discordLink:
-      "https://discord.com/channels/969228248552706078/1237308430293663744/1237308431719989291",
-  },
-  {
     id: 63,
     discordLink:
       "https://discord.com/channels/969228248552706078/1230090463361499147/1230090505690284084",
   },
+  {
+    id: 64,
+    discordLink:
+      "https://discord.com/channels/969228248552706078/1240218807973183598/1240218809709760532",
+  }
 ];
