6e9317e21c110a90aa32e06180bda0dec4599717
Ondřej Sojka <8470346+tensojka@users.noreply.github.com>
2024-06-05 12:08:56 +0200

    chore: update proposals with 68

--- src/components/Proposal/fetchProposal.ts
+++ src/components/Proposal/fetchProposal.ts
@@ -2,8 +2,8 @@ import { Proposal } from "../../types/proposal";
 
 export const activeProposal: Proposal[] = [
   {
-    id: 67,
+    id: 68,
     discordLink:
-      "https://discord.com/channels/969228248552706078/1245285012312428554/1245285013541486613",
+      "https://discord.com/channels/969228248552706078/1247854221303349380/1247854223631323136",
   },
 ];
