b275333fc484f8a07e19f04cb411badf7153664d
DaveVodrazka <dave.vodrazka@gmail.com>
2024-06-11 00:37:59 +0200

    feat: update proposals

--- src/components/Proposal/fetchProposal.ts
+++ src/components/Proposal/fetchProposal.ts
@@ -6,4 +6,9 @@ export const activeProposal: Proposal[] = [
     discordLink:
       "https://discord.com/channels/969228248552706078/1247854221303349380/1247854223631323136",
   },
+  {
+    id: 69,
+    discordLink:
+      "https://discord.com/channels/969228248552706078/1249692546335248415/1249692547572301886",
+  },
 ];
