9c88175252b8ccab47cae2fff45f60a362f2e468
Ondřej Sojka <8470346+tensojka@users.noreply.github.com>
2024-06-18 11:30:42 +0300

    Update live proposals

--- src/components/Proposal/fetchProposal.ts
+++ src/components/Proposal/fetchProposal.ts
@@ -2,18 +2,13 @@ import { Proposal } from "../../types/proposal";
 
 export const activeProposal: Proposal[] = [
   {
-    id: 68,
-    discordLink:
-      "https://discord.com/channels/969228248552706078/1247854221303349380/1247854223631323136",
-  },
-  {
-    id: 69,
-    discordLink:
-      "https://discord.com/channels/969228248552706078/1249692546335248415/1249692547572301886",
-  },
-  {
     id: 70,
     discordLink:
       "https://discord.com/channels/969228248552706078/1250365815036051579/1250365816889675887",
+  },
+  {
+    id: 71,
+    discordLink:
+      "https://discord.com/channels/969228248552706078/1252537269408108564/1252537270687240256",
   },
 ];
