53271c58609bb70533b0e0b8d71585fc42a86a88
Ondřej Sojka <8470346+tensojka@users.noreply.github.com>
2024-06-19 17:42:53 +0300

    Add prop 73, remove prop 70

--- src/components/Proposal/fetchProposal.ts
+++ src/components/Proposal/fetchProposal.ts
@@ -2,11 +2,6 @@ import { Proposal } from "../../types/proposal";
 
 export const activeProposal: Proposal[] = [
   {
-    id: 70,
-    discordLink:
-      "https://discord.com/channels/969228248552706078/1250365815036051579/1250365816889675887",
-  },
-  {
     id: 71,
     discordLink:
       "https://discord.com/channels/969228248552706078/1252537269408108564/1252537270687240256",
@@ -15,5 +10,10 @@ export const activeProposal: Proposal[] = [
     id: 72,
     discordLink:
       "https://discord.com/channels/969228248552706078/1252638634847244359/1252640251831255142",
+  },
+  {
+    id: 73,
+    discordLink:
+      "https://discord.com/channels/969228248552706078/1252990513074798625/1252990516044496916",
   }
 ];
